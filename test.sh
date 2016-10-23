718ca3bbf9f8a652ef353e6f6bd45c1c39d78e0a
mblet <mblet@mblets-Mac.local>
2016-10-23 12:31:59 +0200

    2016-10-23 12:31:55 .test

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 #    By: mblet <mblet@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/10/23 12:09:53 by mblet             #+#    #+#              #
-#    Updated: 2016/10/23 12:29:06 by mblet            ###   ########.fr        #
+#    Updated: 2016/10/23 12:31:41 by mblet            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,11 +20,11 @@ champion4=./champions/examples/helltrain.cor
 champions/corewar/corewar \
 	-v 4 \
 	-d $dump \
-	$champion1 $champion2 $champion3 $champion4 > test_dump_zaz
+	$champion1 $champion2 $champion3 $champion4 > "test_dump_zaz.test"
 
 ./corewar \
 	--ops \
 	-d $dump \
-	$champion1 $champion2 $champion3 $champion4 > test_dump_me
+	$champion1 $champion2 $champion3 $champion4 > "test_dump_me.test"
 
-diff -s --speed-large-files test_dump_zaz test_dump_me | sed "s/^</me :/" | sed "s/^>/zaz:/"
+diff -s --speed-large-files "test_dump_zaz.test" "test_dump_me.test" | sed "s/^</me :/" | sed "s/^>/zaz:/"
