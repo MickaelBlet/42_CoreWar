6455bc003b33a7426769707f7bc07bdb5ea21531
mblet <mblet@mblets-Mac.local>
2016-10-30 17:06:24 +0100

    2016-10-30 17:06:21 comment

--- vm_dir/srcs/op/aff.c
+++ vm_dir/srcs/op/aff.c
@@ -6,7 +6,7 @@
 /*   By: mblet <mblet@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/27 12:08:55 by mblet             #+#    #+#             */
-/*   Updated: 2016/10/30 13:55:41 by mblet            ###   ########.fr       */
+/*   Updated: 2016/10/30 17:06:17 by mblet            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	op_aff(t_process *process, int type[4], int arg[4])
 {
 	(void)type;
-	ft_printf("cycle %i: %i ", sgt_corewar()->cycle, process->uid);
+	//ft_printf("cycle %i: %i ", sgt_corewar()->cycle, process->uid);
 	if (sgt_corewar()->option.verbose.aff == true)
 		ft_putchar(process->reg[arg[0] - 1] % 256);
-	ft_putchar('\n');
+	//ft_putchar('\n');
 }
